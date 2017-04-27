bd035ff462f02de7e55661fd9462ea8946261b53
Matthew James Hodgson <matthodg@uvic.ca>
2017-04-27 14:13:49 -0700

    Added unity files

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PaddleController : MonoBehaviour
+{
+    private float minY = -3.75f;
+    private float maxY = 3.75f;
+
+    public float speed = 10;
+
+    void Update()
+    {
+        MovePaddle();
+    }
+
+    private void MovePaddle()
+    {
+        float movement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        Vector2 position = transform.position;
+        position.y = Mathf.Clamp(position.y + movement, minY, maxY);
+        transform.position = position;
+    }
+}
